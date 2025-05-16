840ada90b366d258cd285c109ba11fa6e1cd9521
Felgy1 <jmeza9799@sdsu.edu>
2025-05-15 21:39:52 -0700

    Assignment complete

--- Readme.md
+++ Readme.md
@@ -2,10 +2,11 @@
 # Prim's Algorithm with Min Heap and Adjacency Matrix
 
 ## Author
-Your Name Here
+Jose Ramos
 
 ## Description
 Briefly describe what your program does and how the code is structured.
+My program implements Prim's algorithm to create a Minimum Spanning Tree using an Adjacency Matrix graph and a custom Min Heap. main.cpp creates Graph objects and calls on primMST() to print the Minimum Spanning Tree and its total weight. graph.h hold the implementations of the functions that create the Adjacency Matrix graphs, adds edges, and builds/prints the MST using heap.h, which holds the implementations for functions to add a vertex, extract the smallest key, update/reposition vertex keys, restore heap if violated, and check heaps.
 
 ## Files
 - graph.h / graph.cpp
@@ -20,15 +21,26 @@ Run directly from the IDE.
 ## Time Complexity Analysis
 
 
-| Operation            | Time Complexity   |
-|----------------------|-------------------|
-| Insert in MinHeap    |                   |
-| Extract Min          |                   |
-| Decrease Key         |                   |
-| Prim’s MST Overall   |                   |
+| Operation            | Time Complexity |
+|----------------------|-----------------|
+| Insert in MinHeap    | log v           |
+| Extract Min          | log v           |
+| Decrease Key         | log v           |
+| Prim’s MST Overall   | v^2 log v       |
 
 _Explain why your MST implementation has the above runtime._
+My MST implementation includes a nested loop which is responsible for the n^2 considering it runs depending on the number of vertices. Within those loops, it also calls two heap functions, each which have a time complexity of log n, resulting in an overall time complexity of n^2 log n.
 
 ## Test Case Description
 
-Input:  
+Input: 
+0-1, 2
+0-3, 6
+1-2, 3
+1-3, 8
+1-4, 5
+2-4, 7
+3-4, 9
+
+Output:
+16 as total and smallest weight
--- graph.h
+++ graph.h
@@ -1,12 +1,78 @@
 #ifndef GRAPH_H
 #define GRAPH_H
+#include "heap.h"
 
 class Graph {
 public:
-    Graph(int vertices);
-    ~Graph();
-    void addEdge(int u, int v, int weight);
-    void primMST();  // Must print MST edges and total weight
+    Graph(int vertices) {
+        numVertices = vertices;
+
+        adjMatrix = new int*[vertices]; //Creates adjacency matrix
+        for (int i = 0; i < vertices; i++) { //Created arrays in matrix
+            adjMatrix[i] = new int[vertices];
+
+            for (int j = 0; j < vertices; j++) { //initializes edges to 0
+                adjMatrix[i][j] = 0;
+            }
+        }
+    }
+
+    ~Graph() {
+        for (int i = 0; i < numVertices; i++) { //deletes the adjacency matrix
+            delete[] adjMatrix[i];
+        }
+        delete[] adjMatrix;
+    }
+
+    void addEdge(int u, int v, int weight) {
+        adjMatrix[u][v] = weight;
+        adjMatrix[v][u] = weight;
+    }
+
+    void primMST() {
+        int* key = new int[numVertices]; //stores min weight to add each vertex
+        int* parent = new int[numVertices]; //stores MST structure
+        bool* inMST = new bool[numVertices]; //tracks vertices included
+
+        MinHeap minHeap(numVertices);
+
+        for (int v = 0; v < numVertices; v++) { //initializes keys and MST
+            key[v] = INT_MAX;
+            parent[v] = -1;
+            inMST[v] = false;
+        }
+
+        key[0] = 0; //starts at vertex 0
+        for (int v = 0; v < numVertices; v++) {
+            minHeap.insert(v, key[v]);
+        }
+
+        std::cout << "Edge \tWeight\n";
+        int totalWeight = 0;
+
+        while (!minHeap.isEmpty()) {
+            int u = minHeap.extractMin(); //vertex with smallest key value
+            inMST[u] = true;
+
+            for (int v = 0; v < numVertices; v++) { //runs for every adjacent vertices v of u
+                if (adjMatrix[u][v] && !inMST[v] && adjMatrix[u][v] < key[v]) {
+                    key[v] = adjMatrix[u][v];
+                    parent[v] = u;
+                    minHeap.decreaseKey(v, key[v]);
+                }
+            }
+        }
+        for (int i = 1; i < numVertices; i++) { //prints edges
+            std::cout << parent[i] << " - " << i << "\t" << adjMatrix[i][parent[i]] << "\n";
+            totalWeight += adjMatrix[i][parent[i]];
+        }
+
+        std::cout << "Total Weight: " << totalWeight << std::endl;
+
+        delete[] key;
+        delete[] parent;
+        delete[] inMST;
+    }// Must print MST edges and total weight
 
 private:
     int** adjMatrix;
